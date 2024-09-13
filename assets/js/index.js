e34757e39b72f4e2adf59c1947aaf0d6dec5c8b7
Artem Derkachevskyi <derkachevskyiartem@gmail.com>
2024-09-13 21:16:50 +0300

    Add tasc solution

---
+++ assets/js/index.js
@@ -0,0 +1,102 @@
+// // оператори
+
+// // typeof оператор визначення типу данних
+
+// typeof 10;
+
+// let data = "test";
+// console.log(typeof data);
+
+// let bug = null;
+// console.log(typeof bug); // object
+
+// // Математичні оператори
+// // +, -, *, /
+
+// console.log("2 + 2");
+// console.log(2 + 2); // нормальна математика
+// console.log(true + true + true); // 3
+// // конкатенація рядків
+// console.log("" + 10 + 20 + 30);
+// console.log("" + null);
+
+// console.log("2 * 10");
+// console.log(2 * 10);
+
+// console.log("100 / 4");
+// console.log(100 / 4);
+
+// // Зведення в ступінь **
+
+// console.log("5 ** 2");
+// console.log(5 ** 2);
+
+// console.log("-5 ** 2");
+// console.log((-5) ** 2);
+
+// // залишок від ділення
+
+// console.log("10 % 3");
+// console.log(10 % 3);
+
+// // інкримент та дикримент
+
+// let counter = 0;
+
+// counter++;
+// counter++;
+// counter++;
+// console.log(counter);
+
+// counter--;
+// console.log(counter);
+
+// // counter++ -- постфіксний
+// // counter++ -- префіксний
+
+// let counter1 = 10;
+
+// console.log(++counter1); // 11
+// console.log(counter1); // 11
+// console.log(++counter1); // 12
+
+// let counter2 = 10;
+// console.log(counter2++); // 10
+// console.log(counter2); // 11
+// console.log(counter2++); // 12
+// console.log(counter2); // 12
+
+// // унарні операиори +, -
+
+// console.log(-10);
+// console.log(+500);
+// console.log(-"675.5");
+
+// console.log(-"asdfasdfasdfsa");
+// console.log(+"1285");
+
+// // перетворення на числовий тип
+// console.log(+null);
+
+// //
+
+let num1 = 70;
+let num2 = 30;
+let sum = num1 + num2;
+console.log(sum);
+
+let minusResult = sum - 50;
+console.log(minusResult);
+
+let multiplyResult  = minusResult * sum;
+console.log(multiplyResult);
+
+let divideResult = multiplyResult / 2.687;
+console.log(divideResult)
+
+let remainder  = multiplyResult % 2.687;
+console.log(remainder)
+
+let stringResult = '' + divideResult;
+console.log(stringResult)
+
